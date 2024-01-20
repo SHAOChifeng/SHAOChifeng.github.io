2ca12ff518da48c5832ec4bc007ab2861dafd545
Chifeng SHAO <59716157+SHAOChifeng@users.noreply.github.com>
2024-01-20 14:20:38 +0800

    1

---
+++ static/js/main3.js
@@ -0,0 +1,96 @@
+ (function($) {
+    "use strict";
+     $(document).on('ready', function() {
+	
+		/*====================================
+			Search JS
+		======================================*/ 
+		$('.search a').on( "click", function(){
+			$('.search-top').toggleClass('active');
+		});
+		
+		/*====================================
+			Single Portfolio Slider JS
+		======================================*/ 
+		$('.pf-details-slider').owlCarousel({
+			items:1,
+			autoplay:false,
+			autoplayTimeout:5000,
+			smartSpeed: 400,
+			autoplayHoverPause:true,
+			loop:true,
+			merge:true,
+			nav:true,
+			dots:false,
+			navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+		});
+		
+		/*=====================================
+			Counter Up JS
+		======================================*/
+		$('.counter').counterUp({
+			delay:20,
+			time:2000
+		});
+		/*====================================
+			Mobile Menu
+		======================================*/ 	
+		$('.mediplus-nav').slicknav({
+			prependTo:".mobile-nav",
+			duration: 300,
+			closeOnClick:true,
+		});
+	
+		
+
+	});
+		
+		$.scrollUp({
+			scrollName: 'scrollUp',      // Element ID
+			scrollDistance: 300,         // Distance from top/bottom before showing element (px)
+			scrollFrom: 'top',           // 'top' or 'bottom'
+			scrollSpeed: 1000,            // Speed back to top (ms)
+			animationSpeed: 200,         // Animation speed (ms)
+			scrollTrigger: false,        // Set a custom triggering element. Can be an HTML string or jQuery object
+			scrollTarget: false,         // Set a custom target element for scrolling to. Can be element or number
+			scrollText: ["<i class='fa fa-angle-up'></i>"], // Text for element, can contain HTML
+			scrollTitle: false,          // Set a custom <a> title if required.
+			scrollImg: false,            // Set true to use image
+			activeOverlay: false,        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+			zIndex: 2147483647           // Z-Index for the overlay
+		});
+		/*=====================
+			Video Popup JS
+		=======================*/ 
+		$('.video-popup').magnificPopup({
+			type: 'video',	
+		});
+		
+
+		/*===================
+			Accordion JS
+		=====================*/ 
+		$('.accordion > li:eq(0) a').addClass('active').next().slideDown();
+		$('.accordion a').on('click', function(j) {
+			var dropDown = $(this).closest('li').find('p');
+			$(this).closest('.accordion').find('p').not(dropDown).slideUp(300);
+			if ($(this).hasClass('active')) {
+				$(this).removeClass('active');
+			} else {
+				$(this).closest('.accordion').find('a.active').removeClass('active');
+				$(this).addClass('active');
+			}
+			dropDown.stop(false, true).slideToggle(300);
+			j.preventDefault();
+		});
+		
+		
+	/*====================
+		Preloader JS
+	======================*/
+	$(window).on('load', function() {
+		$('.preloader').addClass('preloader-deactivate');
+	});
+	
+	
+})(jQuery);
